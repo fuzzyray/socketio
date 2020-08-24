6f8c3f9a774a783d36b4c2967187b5aa8bf5d92f
Paul Varner <fuzzyray@gmail.com>
2020-08-24 14:26:32 -0500

    Initial import from https://github.com/freeCodeCamp/boilerplate-socketio/
    * Updated package dependencies
    * Updated server.js for Mongo 3

---
+++ server.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const auth = require('./app/auth.js');
+const routes = require('./app/routes.js');
+const mongo = require('mongodb').MongoClient;
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const app = express();
+const http = require('http').Server(app);
+const sessionStore = new session.MemoryStore();
+
+fccTesting(app); //For FCC testing purposes
+
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'pug');
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  key: 'express.sid',
+  store: sessionStore,
+}));
+
+const connectOptions = {
+  useUnifiedTopology: true,
+  retryWrites: true,
+  w: 'majority',
+};
+const mongoClient = new mongo(process.env.DATABASE, connectOptions);
+mongoClient.connect((err) => {
+  if (err) {
+    console.log('Database error: ' + err);
+  } else {
+    const db = mongoClient.db(process.env.DBNAME);
+    console.log('Successful database connection');
+
+    auth(app, db);
+    routes(app, db);
+
+    http.listen(process.env.PORT || 3000);
+
+    //start socket.io code
+
+    //end socket.io code
+  }
+});
